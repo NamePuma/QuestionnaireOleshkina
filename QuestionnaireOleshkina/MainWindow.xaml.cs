df1ebc0781d57fc8776da9887f7765f920354030
student <student@K21-07-STUDENT>
2023-02-28 09:18:28 +0500

    Добавьте файлы проекта.

---
+++ QuestionnaireOleshkina/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuestionnaireOleshkina
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    
+
+    public partial class MainWindow : Window
+    {
+
+        private Page enterSistem = new EnterInSistem();
+
+        public Page EnterSistem
+        {
+            get { return enterSistem; }
+        }
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyFrame.Navigate(EnterSistem);
+                
+
+        }
+    }
+}
